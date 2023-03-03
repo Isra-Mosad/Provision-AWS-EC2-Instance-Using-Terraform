19bcaca239408935ca80cff573be392bfb311631
isra-mosad <esrmosaad@gmail.com>
2023-03-03 15:15:50 +0200

    second commit

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_eip" "my-eip"{
   vpc = true
 }
 resource "aws_eip_association" "associate"{
- instance_id=aws_instance.my-ec2.id
+ instance_id=aws_instance.my_ec2.id
  allocation_id=aws_eip.my-eip.id
 
 }
