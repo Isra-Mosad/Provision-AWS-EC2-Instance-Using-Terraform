460c345c5f8c31d2277065a4010552f3bd23fd8d
isra-mosad <esrmosaad@gmail.com>
2023-03-03 16:11:59 +0200

    third commit

--- main.tf
+++ main.tf
@@ -14,6 +14,24 @@ resource "aws_eip_association" "associate"{
 
 }
 
+output "eip_value" {
+ description = "VMs Private IP"
+ value= aws_instance.my_ec2.public_ip
+
+}
+output "ec2_name" {
+ description = "VM IP"
+ value= aws_instance.my_ec2.tags.Name
+
+}
+output "ec2_ami" {
+ description = "VM IP"
+ value= aws_instance.my_ec2.ami
+
+}
+
+
+
 
 
 
--- provider.tf
+++ provider.tf
@@ -12,3 +12,15 @@ terraform {
 provider "aws" {
     region = "us-west-1"
 }
+
+terraform {
+    backend "s3" {
+        bucket = "israa--bucket"
+        key = "terraform.tfstate"
+        region = "us-west-1"
+    }
+    
+ 
+
+ 
+}
